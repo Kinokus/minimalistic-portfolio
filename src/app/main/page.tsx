bf0bd25f83a6927fa27336c46db07d6f7d590e20
Aleks Kinokus <aleks.kinokus@gmail.com>
2025-04-16 15:57:53 +0300

    tailwind works

---
+++ src/app/main/page.tsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <div className="grid grid-cols-2 gap-4 p-4 "> {/* Apply Tailwind grid styles */}
+            <h1 className="text-2xl font-bold">Main Page</h1>
+            <p className="text-lg">Welcome to the main page of our application!</p>
+            <button className="btn-custom">test</button>
+        </div>
+    );
+}
